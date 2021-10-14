2dcc83b6707a3b4388b103430262bee4dc7769b6
float <gxhurry@outlook.com>
2021-10-14 21:26:10 +0800

    JS Updated loop

--- JavaScript/aaa.js
+++ JavaScript/aaa.js
@@ -1,10 +1,5 @@
-let cats = ['Bill', 'Jeff', 'Pete', 'Biggles', 'Jasmin'];
-let cur;
-for (let i = 0; i < cats.length; i++) {
-    console.log("i = " + i);
-    if ('Jeff' === cats[i] ) {
-        cur = i;
-        break;
-    }
+let fibonacci = [1, 1];
+for (let i = 2; i < 45; i++) {
+    fibonacci[i] = fibonacci[i-2] + fibonacci[i-1];
 }
-console.log(cur);
+console.log(fibonacci);

da8a289ded9d3e757a3b43bae08adee4f0381554
float <gxhurry@outlook.com>
2021-10-27 21:34:18 +0800

    Update

--- JavaScript/aaa.js
+++ JavaScript/aaa.js
@@ -1,7 +1,21 @@
-let cat = {
+let cat1 = {
     name: "Ket",
     say: function() {
         console.log("My name is " + this.name);
     }
 }
-cat.say();
+let cat2 = Object.create(cat1);
+cat2.name = "Bill";
+cat1.say();
+cat2.say();
+
+let badCat1 = { // 一个不好的例子
+    name: "Steve",
+    say: function() {
+        console.log("My name is " + badCat1.name); 
+    } 
+};
+let badCat2 = Object.create(badCat1);
+badCat2.name = "重干";
+badCat1.say();
+badCat2.say();
